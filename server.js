0ed8d8e2407064c7b3589fe6568507fc5b4fc9fe
rawera_daniel <rawera.daniel@gmail.com>
2024-06-05 19:05:27 +0300

    feat: add mongoose schema and model to describe structure of the tour data

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
-const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
+
+// To specify a schema for our data
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, require: [true, 'A tour must have a price'] },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
