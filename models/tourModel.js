7ed585e9f9e08e4b1f5d8cc9702296f8b72816de
rawera_daniel <rawera.daniel@gmail.com>
2024-06-17 17:04:25 +0300

    feat: add pre-find & post-find middleware to manipulate data before & after saving to database

--- models/tourModel.js
+++ models/tourModel.js
@@ -47,6 +47,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -69,6 +73,20 @@ tourSchema.pre('save', function (next) {
 //   console.log(doc);
 //   next();
 // });
+
+// Query Middleware
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!
+`);
+  next();
+});
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
