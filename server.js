d704e1b881e4fe9de4da8568e2fec778233d29c8
rawera_daniel <rawera.daniel@gmail.com>
2024-06-20 09:58:23 +0300

    feat: create handler function to manage unfound routes

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const DB = process.env.DATABASE.replace(
 
 mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}....`);
 });
