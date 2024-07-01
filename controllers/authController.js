30844e74b033783bd09155d499c41d86e40e395b
rawera_daniel <rawera.daniel@gmail.com>
2024-07-01 19:29:01 +0300

    feat: send JSON Web Token via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,20 @@ const signToken = (id) =>
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  // Send cookie
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from output (res) when first created.
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
