a483d518a2ab98b17b0ee04e0a54ef93df3929f1
rawera_daniel <rawera.daniel@gmail.com>
2024-07-01 17:02:05 +0300

    feat: allow users to delete their accounts

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,7 @@ const {
   updateUser,
   deleteUser,
   updateMe,
+  deleteMe,
 } = require('../controllers/userController');
 
 const {
@@ -27,6 +28,7 @@ router.patch('/resetPassword/:token', resetPassword);
 
 router.patch('/updateMyPassword', protect, updatePassword);
 router.patch('/updateMe', protect, updateMe);
+router.delete('/deleteMe', protect, deleteMe);
 
 router.route('/').get(getAllUsers).post(createUser);
 
