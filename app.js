360c5fe02154bf6f8fb7449705f8490ebcbd6087
rawera_daniel <rawera.daniel@gmail.com>
2024-07-02 16:24:26 +0300

    feat: set security HTTP headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,20 +11,31 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) Global Middleware
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Limit requests from same API
 const limiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // 15 minutes
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  windowMs: 60 * 60 * 1000, // 1hr
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 1hr)
   message: 'Too many requests from this IP, please try again in an hour!',
 });
 
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
 
